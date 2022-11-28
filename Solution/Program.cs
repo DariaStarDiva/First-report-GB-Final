5a1f3b1756578fd3e0668c5b2647d4d599c473b2
Daria <dasha_starikova@mail.ru>
2022-11-28 13:28:57 +0300

    solution

--- Solution/Program.cs
+++ Solution/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 
+
+Console.Clear();
+
+Console.Write("Сколько элементов Вы хотите ввести? ");
+int size  = Convert.ToInt32(Console.ReadLine());
+
+string[] arrayStrings = new string[size];
+for (int  i = 0; i < size; i++)
+{
+    Console.WriteLine($"Введите {i+1}-й элемент: ");
+    string element=Convert.ToString(Console.ReadLine());
+    arrayStrings[i] = element;
+}
+
+string [] arrayFinal = new string[size];
+
+int count = 0;
+
+for (int  j = 0; j < size; j ++)
+{
+    if (arrayStrings[j].Length <= 3)
+    {
+        arrayFinal[count] = arrayStrings[j];
+        ++count;
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int  i = 0; i < array.Length; i++)
+    {
+        Console.Write(array [i] + " /");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Обработанный массив");
+PrintArray(arrayFinal);
